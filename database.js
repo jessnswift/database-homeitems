eb56ed6ca9acf8ee046a160e1175b224697f4868
Jessica Swift <jessica.nicole.swift@gmail.com>
2018-04-09 23:07:03 -0500

    Added  a function that performs the operation of creating your DOM components

--- database.js
+++ database.js
@@ -1,37 +1,50 @@
 // Access your data from the local storage
 const inventory = JSON.parse(localStorage.getItem("HomeInventory"));
 
-// Get reference to article to append the sections to it
-var articleElement = document.getElementById("myStuff");
+// Write a function that performs the operation of creating DOM components
+function buildInventoryPage(categoryFilter) {
 
-// Iterate over each key in the inventory
-for (let category in inventory) {
+    // Get reference to article to append the sections to it
+    var articleElement = document.getElementById("myStuff");
 
-    // Create a section for each inventory category
-    const sectionElement = document.createElement("section");
+    // Iterate over each key in the inventory
+    for (let category in inventory) {
 
-    // Create a paragraph tag for each property on each item
-    let categoryArray = inventory[category];
-    categoryArray.forEach(function(inventoryItem) {
+        // If current category matches the category filter or if no catergory
+        // filter is provided then build the elements
+        if (category === categoryFilter || !categoryFilter) {
 
-        for (let property in inventoryItem) {
-            const detailParagraph = document.createElement("p");
 
-            // Create textnode with each name, location, descritption
-            var paragraphText = document.createTextNode(inventoryItem[property]);
+            // Create a section for each inventory category
+            const sectionElement = document.createElement("section");
 
-            // Attach the textnode to p
-            detailParagraph.appendChild(paragraphText);
-                    
-            // Attach the p componets as children of the section
-            sectionElement.appendChild(detailParagraph);
+            // Create a paragraph tag for each property on each item
+            let categoryArray = inventory[category];
+            categoryArray.forEach(function (inventoryItem) {
+
+                for (let property in inventoryItem) {
+                    const detailParagraph = document.createElement("p");
+
+                    // Create textnode with each name, location, descritption
+                    var paragraphText = document.createTextNode(inventoryItem[property]);
+
+                    // Attach the textnode to p
+                    detailParagraph.appendChild(paragraphText);
+
+                    // Attach the p componets as children of the section
+                    sectionElement.appendChild(detailParagraph);
+
+                }
+            })
+
+            // Attach the section as a child of the article
+            articleElement.appendChild(sectionElement);
 
         }
-    })
-
-    // Attach the section as a child of the article
-    articleElement.appendChild(sectionElement);
+    }
 }
+
+buildInventoryPage("crafts");
 
 
 
